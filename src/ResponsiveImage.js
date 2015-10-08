4d5077cfe18ee82385941b35d98dfeaeed254f5c
puppybits <bobby@levelmoney.com>
2015-10-08 15:48:28 -0700

    [API] update api to match resize-image-loader
    srcset will take either a string or an object from that has:
    {
      placeholder: '',
      srcset: '',
      size: (width: num, height: num}
    }

--- src/ResponsiveImage.js
+++ src/ResponsiveImage.js
@@ -7,11 +7,13 @@ module.exports = React.createClass({
 
   propsTypes: {
     src: React.PropTypes.string.isRequired,
-    srcset: React.PropTypes.string.isRequired,
+    srcset: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.object]).isRequired,
     placeholder: React.PropTypes.object,
   },
 
-  getDefaultProps() {
+  getDefaultProps: function() {
     return {
       src: null,
       srcset: null,
@@ -19,30 +21,38 @@ module.exports = React.createClass({
     };
   },
 
-	getInitialState: () => ({loaded:false}),
+	getInitialState: function(){
+    return {loaded:false};
+  },
 
-	render() {
+	render:function() {
+
+    var isObject = typeof this.props.srcset === "object",
+      srcset = isObject ? this.props.srcset.srcset : this.props.srcset,
+      placeholder = isObject ? this.props.srcset.placeholder : this.props.placeholder;
 
     var coverStyle = {
       top: '0',
       left: '0',
       width: '100%'};
 
-    var placeholder = {className:'placeholder',
-      src:this.props.placeholder,
+    var temp = {className:'placeholder',
+      src:placeholder,
       style:assign({}, {
         opacity:(this.state.loaded ? 0 : 1),
         transition: 'opacity 10ms 3100ms'},
         (this.props.cover ? {width: '100%'} : null))};
 
     var img = assign({}, this.props, {
-      srcSet:this.props.srcSet,
+      srcSet:srcset,
       style:assign({}, {
         opacity:(this.state.loaded ? 1 : 0),
         transition: 'opacity 3000ms',
         position:'absolute'},
         (this.props.cover ? coverStyle : null)),
-      onLoad:() => this.setState({loaded:true})});
+      onLoad:function(){
+        this.setState({loaded:true});
+      }.bind(this)});
     delete img.placeholder;
 
     var style = assign({}, this.props.style);
@@ -50,7 +60,7 @@ module.exports = React.createClass({
 
     return React.createElement('div', {style:style},
       React.createElement('div', {style:{position: 'relative', width:'100vw', minWidth:'100%', minHeight:'100%'}},
-        React.createElement('img', placeholder),
+        React.createElement('img', temp),
         React.createElement('img', img)));
   },
 });
