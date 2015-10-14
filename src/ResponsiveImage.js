8e7cb19c9545c6e3b07a134d5a7280e03e368571
puppybits <bobby@levelmoney.com>
2015-10-14 15:04:02 -0700

    [Fix] minor fixes for cross-fade and remove react warning

--- src/ResponsiveImage.js
+++ src/ResponsiveImage.js
@@ -16,7 +16,7 @@ module.exports = React.createClass({
   getDefaultProps: function() {
     return {
       src: null,
-      srcset: null,
+      srcSet: null,
       placeholder: null,
     };
   },
@@ -25,16 +25,44 @@ module.exports = React.createClass({
     return {loaded:false};
   },
 
-	render:function() {
+  render:function() {
 
     var isObject = typeof this.props.srcset === "object",
       srcset = isObject ? this.props.srcset.srcset : this.props.srcset,
       placeholder = isObject ? this.props.srcset.placeholder : this.props.placeholder;
 
+    if (!placeholder) {
+      return this.renderNoPlaceholder(srcset);
+    }
+
+    return this.renderCover(srcset, placeholder);
+  },
+
+	renderNoPlaceholder:function(srcset) {
+
+    var img = assign({}, this.props, {
+      srcSet:srcset,
+      style:assign({}, this.props.style || {}),
+      onLoad:function(){
+        this.setState({loaded:true});
+      }.bind(this)});
+    delete img.srcset;
+
+    return React.createElement('img', img);
+  },
+
+  renderCover:function(srcset, placeholder) {
+
     var coverStyle = {
       top: '0',
       left: '0',
       width: '100%'};
+    var coverWrapperStyle = {
+      position: 'relative',
+      width:'100vw',
+      minWidth:'100%',
+      minHeight:'100%'
+    };
 
     var temp = {className:'placeholder',
       src:placeholder,
@@ -46,20 +74,21 @@ module.exports = React.createClass({
     var img = assign({}, this.props, {
       srcSet:srcset,
       style:assign({}, {
-        opacity:(this.state.loaded ? 1 : 0),
-        transition: 'opacity 3000ms',
+        opacity:(!placeholder || this.state.loaded ? 1 : 0),
+        transition: (!placeholder ? '' : 'opacity 3000ms'),
         position:'absolute'},
         (this.props.cover ? coverStyle : null)),
       onLoad:function(){
         this.setState({loaded:true});
       }.bind(this)});
+    delete img.srcset;
     delete img.placeholder;
 
     var style = assign({}, this.props.style);
     style.height = style.height || '100%';
 
     return React.createElement('div', {style:style},
-      React.createElement('div', {style:{position: 'relative', width:'100vw', minWidth:'100%', minHeight:'100%'}},
+      React.createElement('div', {style:coverWrapperStyle},
         React.createElement('img', temp),
         React.createElement('img', img)));
   },
